ffe3bbe8de47d07c1ccb7bf32a8cf6d5503d16d4
SongRick <18730271290@163.com>
2025-04-18 15:34:58 +0800

    重设UI，准备增加选址功能

--- Assets/SimCity/Scripts/GRID/HidePanel.cs
+++ Assets/SimCity/Scripts/GRID/HidePanel.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class HidePanel : MonoBehaviour
 {
     private UIInput _UIInput;
-    GameObject targetGameObject;
+    private GameObject targetGameObject;
+    private GameObject gameObjectPanelCreate;
     // 在脚本实例被启用时调用，进行初始化操作
     private void Start()
     {
         gameObject.SetActive(false);
         targetGameObject = GameObject.Find("Player");
+        gameObjectPanelCreate = GameObject.Find("panel_create");
+        gameObjectPanelCreate.SetActive(false);
         if (targetGameObject != null)
         {
             // 获取目标游戏对象上的 UIInput 组件
@@ -26,5 +29,20 @@ public class HidePanel : MonoBehaviour
     {
         gameObject.SetActive(flagEditMode);
     }
-
+    public void showPanelCreate()
+    {
+        if(gameObjectPanelCreate==null)
+        {
+            Debug.LogError("未找到panel_create");
+            return;
+        }
+        if(gameObjectPanelCreate.activeSelf)
+        {
+            gameObjectPanelCreate.SetActive(false);
+        }
+        else
+        {
+            gameObjectPanelCreate.SetActive(true);
+        }
+    }
 }
