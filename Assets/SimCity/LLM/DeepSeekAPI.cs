b82a797d98fde31292a00307a93e5e5520e3f58d
SongRick <18730271290@163.com>
2025-05-08 17:52:36 +0800

    deepseek api基本完成，准备根据json生成建筑

--- Assets/SimCity/LLM/DeepSeekAPI.cs
+++ Assets/SimCity/LLM/DeepSeekAPI.cs
@@ -18,7 +18,7 @@ public class DeepSeekAPI : MonoBehaviour
     // 对话参数
     [Header("Dialogue Settings")]
     [Range(0, 1)] public float temperature = 0.5f;// 控制生成文本的随机性（0-1，值越高越随机）
-    [Range(1, 1000)] public int maxTokens = 100; // 生成的最大令牌数（控制回复长度）
+    [Range(1, 1000)] public int maxTokens = 999; // 生成的最大令牌数（控制回复长度）
 
     // 角色设定
     [System.Serializable]
